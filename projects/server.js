6ab726d82ae1112965e76079826a30bff57c94ed
Vlad Mocanu <vladmoca90@gmail.com>
2026-06-23 20:33:14 +0100

    Can now view the countries and persons APIs in browser

--- projects/server.js
+++ projects/server.js
@@ -1,19 +1,19 @@
 ﻿const express = require("express");
+
 const countriesRouter = require("./countries");
 const personsRouter = require("./persons");
 
 const app = express();
 const PORT = 3000;
 
-// Allow Express to read JSON request bodies
 app.use(express.json());
 
-// Test route
+// Home page
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-// Separate route modules
+// Register route modules
 app.use(countriesRouter);
 app.use(personsRouter);
 
